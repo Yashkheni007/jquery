bf1c542ea3b6f5a98a0a3c2588d166dabc44b773
Yashkheni007 <yashkheni007@gmail.com>
2024-03-19 15:52:49 +0530

    jquery task-3,4,5 create

---
+++ task-3/jquery.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+  var first_array = [1,2,3,4,5];
+  var second_array = [4,5,6,7];
+
+  var merge_array = first_array.concat(second_array);
+  var duplicate = [];
+
+
+});
---
+++ task-5/jquery.js
@@ -0,0 +1,26 @@
+$(document).ready(function(){
+  function addTodo() {
+    var todoText = $("#todo").val();
+    if(todoText !== ""){
+      $("#todo-list").append(`<div id="sub-div"><li>` + todoText + `</li><div id="action-btn"><button id="edit-btn">Edit</button><button id="delete-btn" class="delete-btn" onclick="deleteTodo(this)">Delete</button></div></div>`);
+      $("#todo").val("");
+    } else {
+      alert("Please enter a Todo value!");
+    }
+  }
+
+  $('#add-btn').click(function(){
+    addTodo();
+  });
+
+  $(document).on("click" , "#edit-btn" , function(){
+    var newTodo = prompt("Edit Todo value : ");
+    if(newTodo !== ""){
+
+    }
+  });
+});
+
+function deleteTodo(dlt){
+  $(dlt).closest("#sub-div").remove();
+}
