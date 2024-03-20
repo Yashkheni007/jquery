8c52537c06b5a646b58277023c122052f5eef9da
Yashkheni007 <yashkheni007@gmail.com>
2024-03-20 15:26:24 +0530

    dupilicate array created

--- task-3/jquery.js
+++ task-3/jquery.js
@@ -1,9 +1,15 @@
 $(document).ready(function(){
-  var first_array = [1,2,3,4,5];
-  var second_array = [4,5,6,7];
+  var first_array = [11,32,43,45,57];
+  var second_array = [43,55,66,57];
 
   var merge_array = first_array.concat(second_array);
   var duplicate = [];
 
-
+  merge_array.forEach((element , index) => {
+    if(merge_array.indexOf(element) !== index){
+      duplicate.push(element);
+    }
+  });
+  console.log(duplicate);
+  $("#duplicateArray").text("Duplicate array is : " + duplicate );
 });
