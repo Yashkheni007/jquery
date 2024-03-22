1aa6e3df9a8afce8d2ba9da50f8753756eca6b28
Yashkheni007 <yashkheni007@gmail.com>
2024-03-22 14:53:04 +0530

    create reset funtion

--- task-6/jquery.js
+++ task-6/jquery.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {
   function calculateTip(percentage) {
-    var billAmount = $("#bill").val();
+    var billAmount = parseFloat($("#bill").val());
     var totalPeople = $("#people").val();
 debugger
     if (billAmount === "") {
@@ -23,24 +23,10 @@ debugger
     calculateTip(percentage);
   });
 
-  //for add 40 rupees tip
-  $("#40").on("click" , function(){
-    var billAmount = $("#bill").val();
-    var totalPeople = $("#people").val();
-
-    if (billAmount === "") {
-      alert("Please enter Bill Amount!");
-    }
-    if (totalPeople === "" || totalPeople === 0) {
-      $("#errorMsg").text("Can't be zero");
-    }
-
-    var tipAmount = 40;
-    var tipPerPerson = tipAmount / totalPeople;
-    var totalPerPerson = (billAmount + tipAmount) / totalPeople;
-
-    $("#tipPerPerson").text("₹" + tipPerPerson.toFixed(2));
-    $("#totalPerPerson").text("₹" + totalPerPerson.toFixed(2));
-    calculateTip();
+  $("#reset-btn").on("click" ,function(){
+    $("#bill").val("");
+    $("#people").val("");
+    $("#errorMsg").val("");
+    $(".totalValue").text("₹0.00");
   });
 });
